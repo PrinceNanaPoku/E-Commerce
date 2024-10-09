46c460cb0e253c1ca8128761370f12176397ef78
PrinceNanaPoku <princenanapoku64@gmail.com>
2024-10-09 22:36:51 +0000

    created a drawer to display a logo and other pages

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -36,6 +36,73 @@ class _HomePageState extends State<HomePage> {
       bottomNavigationBar: BottomNavBar(
         onTabChange: (index) => navigateBottomBar(index),
       ),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        leading: Builder(builder: (context) {
+          return IconButton(
+              icon: const Icon(
+                Icons.menu,
+                color: Colors.black,
+              ),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              });
+        }),
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.grey[900],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            //logo
+            Column(
+              children: [
+                DrawerHeader(
+                  child: Image.asset('lib/images/sneaker_logo.png'),
+                ),
+                Divider(
+                  color: Colors.grey[800],
+                ),
+                //other pages
+                const ListTile(
+                  leading: Icon(
+                    Icons.home,
+                    color: Colors.white,
+                  ),
+                  title: Text(
+                    'Home',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                const ListTile(
+                  leading: Icon(
+                    Icons.info,
+                    color: Colors.white,
+                  ),
+                  title: Text(
+                    'About',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+
+            const Padding(
+              padding: EdgeInsets.only(bottom: 20.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.logout,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  'Logout',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
       body: _pages[_selectedIndex],
     );
   }
